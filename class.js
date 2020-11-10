5636d3251edf7948655057a506647bc7f2da6edc
Darrian Brown <darrian.jordan.brown@gmail.com>
2020-11-10 14:18:19 -0600

    cleaned up code added rapid fire

--- class.js
+++ class.js
@@ -1,17 +1,14 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
+const healthBarContainer = document.querySelector('.health-bar-container')
 
 export class Player {
   constructor(x, y, radius, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.color = color;
-    this.speed = 1;
-    this.moveAngle = 0;
-    this.angle = 0
+    this.color = color;    
     this.controls = [];
-    
   }
   
   draw() {
@@ -20,8 +17,16 @@ export class Player {
     context.fillStyle = this.color;
     context.fill()
   }
+
+  destroy() {
+    // removes player from screen without having to redraw
+    this.x = -100;
+    this.y = -100; 
+  }
+
   update() {
     this.draw();
+    // Player movement
     if (this.controls && this.controls[87]) {this.y -= 5; }
     if (this.controls && this.controls[65]) {this.x -= 5; }
     if (this.controls && this.controls[83]) {this.y += 5; }
@@ -59,17 +64,65 @@ export class Enemy {
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
+    
   }
   
-  draw() {
+  draw(player) {
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     context.fillStyle = this.color;
     context.fill()
+
+    //enemy tracking player
+    let dx = player.x - this.x 
+    let dy = player.y - this.y
+    let theta = Math.atan2(dy, dx);
+    // let nx = this.x + Math.cos(theta);
+    // let ny = this.y + Math.sin(theta);
+    this.x += 1 * Math.cos(theta);
+    this.y += 1 * Math.sin(theta);
+
+    // this.x += this.speed * Math.sin(this.angle);
+    // this.y -= this.speed * Math.cos(this.angle);
+
   }
 
-  update() {
-    this.draw();
+  update(player) {
+    this.draw(player);
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
+export class Boss {
+  constructor (x, y, radius, color, velocity, health) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.health = health
+  }
+  
+  draw(player) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill()
+
+    //enemy tracking player
+    let dx = player.x - this.x 
+    let dy = player.y - this.y
+    let theta = Math.atan2(dy, dx);
+    this.x += .5 * Math.cos(theta);
+    this.y += .5 * Math.sin(theta);
+    healthBarContainer.style.top = `${this.y - 30}px`
+    healthBarContainer.style.left = `${this.x}px`
+    
+  }
+
+  update(player) {
+    this.draw(player);
     this.x = this.x + this.velocity.x;
     this.y = this.y + this.velocity.y;
   }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Player, Projectile, Enemy, Particle } from './class.js'
+import { Player, Projectile, Enemy, Boss, Particle } from './class.js'
 const canvas = document.querySelector('canvas');
 
 canvas.width = innerWidth;
@@ -8,6 +8,8 @@ const scoreEl = document.querySelector('#scoreEl');
 const startGameBtn = document.querySelector('#startGameBtn');
 const modalEl = document.querySelector('#modalEl');
 const bigScoreEl = document.querySelector('#bigScoreEl');
+const livesEl = document.querySelector('#lives');
+const healthBarEl = document.querySelector('#test')
 
 const context = canvas.getContext('2d');
 
@@ -18,19 +20,35 @@ let player = new Player(x, y, 10, 'white');
 let projectiles = [];
 let enemies = [];
 let particles = [];
+let bosses = [];
+let kills = 0;
+let mouse = {x: 0 , y: 0}
 
 function init() {
   player = new Player(x, y, 10, 'white');
   projectiles = [];
   enemies = [];
+  bosses = [];
   particles = [];
   score = 0;
+  lives = 3;
+  kills = 0;
+  livesEl.innerHTML = lives;
   scoreEl.innerHTML = score
   bigScoreEl.innerHTML = score
 }
-  
+
+function handleEndGame() {
+  setTimeout(() => {
+    cancelAnimationFrame(animationId);
+    modalEl.style.display = 'flex';
+    bigScoreEl.innerHTML = score;
+  }, 1000)
+}
+
+  let id;
 function spawnEnemies() {
-  setInterval(() => {
+  id = setInterval(() => {
     const radius = Math.random() * (30 - 4) + 4;
     let x;
     let y;
@@ -49,25 +67,27 @@ function spawnEnemies() {
       y: Math.sin(angle)
     }
 
-    enemies.push(new Enemy(x, y, radius, color, velocity));
-    // console.log(enemies);
+    if(kills === 1) {
+      let health = 100
+      enemies = [];
+      bosses.push(new Boss(0, 0, 100, 'red', {x: 0, y: 0}, health));
+      clearInterval(id)   
+    } else {
+      enemies.push(new Enemy(x, y, radius, color, velocity));
+    }
   }, 1000);
-}
-
+  }
+  
 let animationId;
 let score = 0;
+let lives;
 function animate() {
  animationId = requestAnimationFrame(animate)
   context.fillStyle = 'rgba(0, 0, 0, 0.3)';
   context.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  // player.update()
   particles.forEach((particle, index) => {
-    if(particle.alpha <= 0) {
-      particles.splice(index, 1);
-    } else {
-      particle.update();
-    }
+    particle.alpha <= 0 ? particles.splice(index, 1) : particle.update();
   })
   projectiles.forEach((projectile, index) => {
     projectile.update();
@@ -79,27 +99,110 @@ function animate() {
       projectile.y + projectile.radius < 0 || 
       projectile.y - projectile.radius > canvas.height
       ) {
-      setTimeout(() => {
-        projectiles.splice(index, 1);
-      }, 0)
+      setTimeout(() => projectiles.splice(index, 1), 0)
     }
   })
 
-  enemies.forEach((enemy, index) => {
-    enemy.update();
+  
 
-    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
-
-    //end game
-    if(dist - enemy.radius - player.radius < 1) {
-      cancelAnimationFrame(animationId);
-      modalEl.style.display = 'flex';
-      bigScoreEl.innerHTML = score
+  bosses.forEach((boss, index) => {
+    boss.update(player);
+    const bossDist = Math.hypot(player.x - boss.x, player.y - boss.y)
+    if(bossDist - boss.radius - player.radius < 1) {
+      lives -= lives;
+      livesEl.innerHTML = lives;
+      
+      //end game
+      if(lives === 0) {
+        
+        for(let i = 0; i < 60 * 2; i++) {
+          particles.push(new Particle(player.x, player.y, Math.random() * 2, player.color, {
+            x: (Math.random() - 0.5) * (Math.random() * 6),
+            y: (Math.random() - 0.5) * (Math.random() * 6), 
+          }))
+        }
+        player.destroy();
+        handleEndGame();
+      }
     }
 
     projectiles.forEach((projectile, projectileIndex) => {
-      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+      const bossDist = Math.hypot(projectile.x - boss.x, projectile.y - boss.y)
 
+      //when projectile touch boss
+      if(bossDist - boss.radius - projectile.radius < 1) {
+        //create explosions
+        for(let i = 0; i < 10 * 2; i++) {
+          particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, boss.color, {
+            x: (Math.random() - 0.5) * (Math.random() * 6),
+            y: (Math.random() - 0.5) * (Math.random() * 6), 
+          }))
+        }
+  
+        if(boss.health - 1 > 1) {
+          //increase our score 
+          boss.health -= 10;
+          healthBarEl.style.width = `${boss.health}%`
+          console.log(healthBarEl.style.width);
+          score += 100;
+          scoreEl.innerHTML = score;
+  
+          gsap.to(boss, {
+            radius: boss.radius - 10
+          })
+          
+          setTimeout(() => projectiles.splice(projectileIndex, 1), 0);
+        } else {
+          setTimeout(() => {
+            bosses.splice(index, 1);
+            projectiles.splice(projectileIndex, 1);
+          }, 0)
+          handleEndGame();
+        }
+      }
+    })
+  })
+  
+  // TODO - refactor boss code
+  
+  enemies.forEach((enemy, index) => {
+    enemy.update(player);
+
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+    // checks for enemy and player touch
+    if(dist - enemy.radius - player.radius < 1) {
+      // removes life from player
+      lives -= 1;
+      livesEl.innerHTML = lives;
+      
+      //end game
+      if(lives === 0) {
+        
+        for(let i = 0; i < 60 * 2; i++) {
+          particles.push(new Particle(player.x, player.y, Math.random() * 2, player.color, {
+            x: (Math.random() - 0.5) * (Math.random() * 6),
+            y: (Math.random() - 0.5) * (Math.random() * 6), 
+          }))
+        }
+        player.destroy();
+        handleEndGame();
+      }
+
+      //Enemy blowup when touching player 
+      enemies.splice(index, 1);
+      for(let i = 0; i < enemy.radius * 2; i++) {
+        particles.push(new Particle(enemy.x, enemy.y, Math.random() * 2, enemy.color, {
+          x: (Math.random() - 0.5) * (Math.random() * 6),
+          y: (Math.random() - 0.5) * (Math.random() * 6), 
+        }))
+      }
+    }
+
+    projectiles.forEach((projectile, projectileIndex) => {
+      
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+      
       //when projectile touch enemy
       if(dist - enemy.radius - projectile.radius < 1) {
 
@@ -119,12 +222,10 @@ function animate() {
           gsap.to(enemy, {
             radius: enemy.radius - 10
           })
-          setTimeout(() => {
-            projectiles.splice(projectileIndex, 1);
-          }, 0)
+          setTimeout(() => projectiles.splice(projectileIndex, 1), 0);
         } else {
           // remove from scene altogether
-
+          kills += 1;
           score += 100;
           scoreEl.innerHTML = score;
 
@@ -134,20 +235,34 @@ function animate() {
           }, 0)
         }
       }
-
     })
   })
 }
 
-addEventListener('click', (event, ) => {
-  const angle = Math.atan2(event.clientY - player.y, event.clientX - player.x);
+function shoot(angle) {
   const velocity = {
     x: Math.cos(angle) * 5,
     y: Math.sin(angle) * 5
   }
-  
   projectiles.push(new Projectile(player.x, player.y, 5, 'white', velocity));
+}
+
+addEventListener('click', (event) => {
+  const angle = Math.atan2(event.clientY - player.y, event.clientX - player.x);
+    shoot(angle)
 });
+
+let i;
+addEventListener('mousedown', () => {
+  i = setInterval(() => {
+    const angle = Math.atan2(mouse.y - player.y, mouse.x - player.x);
+    shoot(angle)
+  }, 100);
+});
+
+addEventListener('mousemove', (event) => [mouse.x, mouse.y] = [event.clientX, event.clientY])
+
+addEventListener('mouseup', () => clearInterval(i));
 
 startGameBtn.addEventListener('click', () => {
   init();
